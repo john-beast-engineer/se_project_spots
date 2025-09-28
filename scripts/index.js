3462b853ed1db6c08d49a7d7f9832f59d5100860
BeastEngineer <john@bethebeast.ai>
2025-09-28 00:35:43 -0400

    add edit profile form prefill and submission handlers

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,53 @@
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
+
 // Edit Profile Modal Elements
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 // New Post Modal Elements
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const cardImageInput = newPostModal.querySelector("#card-image-input");
+const cardCaptionInput = newPostModal.querySelector("#card-caption-input");
 
-// Edit Profile Modal Listeners
+// Function Definitions
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+
+  console.log("Image URL:", cardImageInput.value);
+  console.log("Caption:", cardCaptionInput.value);
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+// Edit Profile Modal Event Listeners
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -19,7 +55,9 @@ editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-// New Post Modal Listeners
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+// New Post Modal Event Listeners
 newPostButton.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
@@ -27,3 +65,5 @@ newPostButton.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
